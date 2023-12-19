c659a7f5698065c8a9efa66254b101a49b51796f
Cristina <heregacris@gmail.com>
2023-12-19 18:38:26 +0100

    refreshing the page, the timer looks nice

--- src/app/components/pomodoro/components/timer/timer.component.ts
+++ src/app/components/pomodoro/components/timer/timer.component.ts
@@ -19,7 +19,7 @@ import { Subscription, interval } from 'rxjs';
 export class TimerComponent {
   @Input() public currentSection!: string;
   @Input() public sectionsList!: { name: string; time: number }[];
-  @Input() public longBreakFrequency!: number;
+  @Input() public longBreakFrequency: number = 2;
 
   private readonly cdr = inject(ChangeDetectorRef);
   public secondsLeft!: number;
@@ -27,28 +27,38 @@ export class TimerComponent {
   public min!: number;
   public sec!: number;
 
-
-  constructor() {
-
-  }
-
-
-  startTimer() {
+  setTime() {
     switch (this.currentSection) {
       case 'pomodoro':
+        this.min = 25
+        this.sec = 0
         this.secondsLeft = 1500;
         break;
 
       case 'short-break':
+        this.min = 5
+        this.sec = 0
         this.secondsLeft = 300;
         break;
       case 'long-break':
+        this.min = 15
+        this.sec = 0
         this.secondsLeft = 900;
         break;
 
       default:
         break;
     }
+  }
+
+
+  ngOnChanges() {
+    this.setTime()
+  }
+
+
+  startTimer() {
+    this.setTime()
 
     this.min = Math.floor(this.secondsLeft / 60);
     this.sec = this.secondsLeft % 60;
@@ -65,12 +75,10 @@ export class TimerComponent {
   }
 
   pauseTimer(): void {
-    console.log('pauseTimer');
     this.timer.unsubscribe();
   }
 
   resumeTimer(): void {
-    console.log('resumeTimer');
     this.timer = interval(1000).subscribe(() => {
       if (this.secondsLeft > 0) {
         this.secondsLeft--;
@@ -86,19 +94,23 @@ export class TimerComponent {
   private pomodoroCount = 0;
 
   nextSection(): void {
-    console.log('nextSection');
     this.timer.unsubscribe();
     this.currentSectionIndex++;
     if (this.currentSectionIndex >= this.sectionsList.length) {
       this.currentSectionIndex = 0;
     }
     this.currentSection = this.sectionsList[this.currentSectionIndex].name;
+    this.setTime()
+
     if (this.currentSection === 'pomodoro') {
       this.pomodoroCount++;
-      if (this.pomodoroCount === this.longBreakFrequency) {
-        this.currentSection = 'long-break';
-        this.pomodoroCount = 0;
-      }
+      this.secondsLeft = 1500
+   
+    }
+    if (this.pomodoroCount === this.longBreakFrequency) {
+      this.currentSection = 'long-break';
+      this.secondsLeft = 900
+      this.pomodoroCount = 0;
     }
 
     console.log(this.currentSection);
@@ -108,9 +120,10 @@ export class TimerComponent {
   get getMinutes(): void {
     const minutes = Math.floor(this.secondsLeft / 60);
     const sec = this.secondsLeft % 60;
-
+  
     this.min = minutes;
     this.sec = sec;
+    this.cdr.detectChanges();
     return console.log(`${minutes}:${sec}`);
   }
 }
