13be3eae3cf8de6ec302b6fdf473ba04006f2190
Cristina <heregacris@gmail.com>
2023-12-24 15:24:37 +0100

    merge conflicts resolved

--- src/app/components/pomodoro/components/sections/sections.component.ts
+++ src/app/components/pomodoro/components/sections/sections.component.ts
@@ -18,11 +18,7 @@ import { CommonModule } from '@angular/common';
 export class SectionsComponent {
   @Input() public currentSection!: string;
   @Input() public sectionsList!: { name: string; time: number }[];
-<<<<<<< HEAD
   @Output() private sectionMessage: EventEmitter<string> =
-=======
-  @Output() private methodFromChild: EventEmitter<string> =
->>>>>>> 2dfc00e20462808e63b63b384b1016fc201f7c19
     new EventEmitter<string>();
 
   public setSection(message: string): void {
