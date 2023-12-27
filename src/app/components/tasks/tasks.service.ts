5deaf9147bec2b7276d108c7496b76c47ecbbdfa
Cristina <heregacris@gmail.com>
2023-12-27 12:23:03 +0100

    implemented task-form html

--- src/app/components/tasks/tasks.service.ts
+++ src/app/components/tasks/tasks.service.ts
@@ -9,42 +9,15 @@ import { v4 as uuidv4 } from 'uuid';
 export class TasksService {
   
   public tasksList = signal<Task[]>([
-    {
-      id: Date.now(),
-      title: 'fjsajfksaf',
-      description: 'hkkds',
-      isSelected: false,
-      status: TaskStatus.NotDone,
-      editMode: false,
-      pomodoros: { totalPomodoros: 10, donePomodoros: 1 },
-    },
-    {
-      id: Date.now(),
-      title: 'gfdg5re',
-      description: '546',
-      isSelected: true,
-      status: TaskStatus.Done,
-      editMode: false,
-      pomodoros: { totalPomodoros: 2, donePomodoros: 15 },
-    },
-    {
-      id: Date.now(),
-      title: 'fjsajfksaf',
-      description: 'hkkds',
-      isSelected: false,
-      status: TaskStatus.NotDone,
-      editMode: false,
-      pomodoros: { totalPomodoros: 10, donePomodoros: 1 },
-    },
-    {
-      id: Date.now(),
-      title: 'fjsajfksaf',
-      description: 'hkkds',
-      isSelected: false,
-      status: TaskStatus.NotDone,
-      editMode: false,
-      pomodoros: { totalPomodoros: 10, donePomodoros: 1 },
-    },
+    // {
+    //   id: Date.now(),
+    //   title: 'fjsajfksaf',
+    //   description: 'hkkds',
+    //   isSelected: false,
+    //   status: TaskStatus.NotDone,
+    //   editMode: false,
+    //   pomodoros: { totalPomodoros: 10, donePomodoros: 1 },
+    // },
   ]);
 
   // - - [PropSignal] selectedTask
